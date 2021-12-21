9ef00f3f2b9a1eb25ac758ad4b78ee104382264f
Asochka <fadeevafadeeva03@gmail.com>
2021-12-21 11:47:58 +0300

    +

--- Ccupee.cpp
+++ Ccupee.cpp
@@ -3,52 +3,52 @@
 
 class Student {
 public:
-std::string fio;
-int studVar;
-int subGroup;
+	std::string fio;
+	int studVar;
+	int subGroup;
 
 Student()
 {
-this->fio = "";
-this->studVar = 0;
-this->subGroup = 0;
+	this->fio = "";
+	this->studVar = 0;
+	this->subGroup = 0;
 }
 
 Student(const std::string fio1, const int studVar1, const int subGroup1)
 {
-this->fio = fio1;
-this->studVar = studVar1;
-this->subGroup = subGroup1;
+	this->fio = fio1;
+	this->studVar = studVar1;
+	this->subGroup = subGroup1;
 }
 
 Student(const Student &other)
 {
-this->fio = other.fio;
-this->studVar = other.studVar;
-this->subGroup = other.subGroup;
+	this->fio = other.fio;
+	this->studVar = other.studVar;
+	this->subGroup = other.subGroup;
 }
-}
+};
 
 class Group {
 public:
-std::string name;
+	std::string name;
 
 Group()
 {
-this->name = "";
+	this->name = "";
 }
 
 Group(const std::string name1)
 {
-this->name = name1;
+	this->name = name1;
 }
 
-Group(sonst Group &other)
+Group(const Group &other)
 {
-this->name = other.name;
+	this->name = other.name;
 }
 
-}
+};	
 
 int main()
 {
